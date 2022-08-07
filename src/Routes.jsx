17adfb80fb6674d8f42b2f977ef3d15901a7678e
Oleksii Kikot <oleksii.kikot@yahoo.com>
2022-08-07 21:47:53 +0200

    added route for reports page

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -6,16 +6,18 @@ import PrivateRoute from 'shared/components/PrivateRoute';
 
 const AuthPage = lazy(() => import('./pages/AuthPage'));
 const HomePage = lazy(() => import('./pages/HomePage'));
+const ReportsPage = lazy(() => import('./pages/ReportsPage'));
 
 const MyRoutes = () => {
   return (
-    <Suspense fallback={<p>Please wait for some magic to be happened</p>}>
+    <Suspense fallback={<p>Please wait for some magic to happen</p>}>
       <Routes>
         <Route element={<PublicRoute />}>
           <Route path="auth" element={<AuthPage />} />
         </Route>
         <Route element={<PrivateRoute />}>
           <Route path="/" element={<HomePage />} />
+          <Route path="reports" element={<ReportsPage />} />
         </Route>
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
