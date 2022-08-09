986ceef96490effc6ac67ff0f47ddd98b644785e
Oleksii Kikot <oleksii.kikot@yahoo.com>
2022-08-09 14:13:57 +0200

    added reports icon snippet

---
+++ src/shared/components/ReportsIcon/ReportsIcon.jsx
@@ -0,0 +1,13 @@
+import styles from './ReportsIcon.module.scss';
+
+const ReportsIcon = () => {
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.element}></div>
+      <div className={styles.element}></div>
+      <div className={styles.element}></div>
+    </div>
+  );
+};
+
+export default ReportsIcon;
