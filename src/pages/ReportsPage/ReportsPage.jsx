41dfcc2811213b52b1588dd1ea8f54742ced060a
BohdanDeveloper <radchukyou1999@gmail.com>
2022-08-12 16:34:18 +0300

    Created new component for total income and expenses

--- src/pages/ReportsPage/ReportsPage.jsx
+++ src/pages/ReportsPage/ReportsPage.jsx
@@ -1,15 +1,30 @@
 // import styles from './ReportsPage.module.scss';
 import CurrentPeriod from '../../components/CurrentPeriod';
+import ExpensesIncomeSwitch from '../../components/ExpensesIncomeSwitch';
+import ExpensesIncome from '../../components/ExpensesIncome';
 import moment from 'moment';
 import { useState } from 'react';
 
 const ReportsPage = () => {
   const [period, setPeriod] = useState(moment().month());
+  const [areExpensesOpen, setAreExpensesOpen] = useState(true);
+  const [totalExpenses, setTotalExpenses] = useState(0);
+  const [totalIncome, setTotalIncome] = useState(0);
 
   return (
-    <div>
-      <CurrentPeriod period={period} setPeriod={setPeriod} />
-    </div>
+    <section>
+      <div className="container">
+        <CurrentPeriod period={period} setPeriod={setPeriod} />
+        <ExpensesIncome
+          totalIncome={totalIncome}
+          totalExpenses={totalExpenses}
+        />
+        <ExpensesIncomeSwitch
+          areExpensesOpen={areExpensesOpen}
+          switchExpenses={setAreExpensesOpen}
+        />
+      </div>
+    </section>
   );
 };
 
