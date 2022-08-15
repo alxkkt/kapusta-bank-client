cb3108ae918d3759e6074b4b3028a350015d7428
Oleksii Kikot <oleksii.kikot@yahoo.com>
2022-08-15 17:22:23 +0200

    deleted unused auth selectors

--- src/redux/auth/auth-selectors.js
+++ src/redux/auth/auth-selectors.js
@@ -1,5 +1,2 @@
 export const isUserLogin = ({ auth }) => auth.isLogin;
 export const isUserEmail = ({ auth }) => auth.userData.email;
-export const getUserName = ({ auth }) => auth.userData.name;
-export const getTotalBalance = ({ auth }) => auth.userData.totalBalance;
-export const getUserToken = ({ auth }) => auth.userData.token;
