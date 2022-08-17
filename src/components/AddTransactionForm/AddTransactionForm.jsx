e8bd05174265b68ab06ad0e2da7aec6c6b08e25b
Pavel Lobtsov <plobtsov@gmail.com>
2022-08-17 21:57:31 +0300

    Background added

--- src/components/AddTransactionForm/AddTransactionForm.jsx
+++ src/components/AddTransactionForm/AddTransactionForm.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import NumberFormat from 'react-number-format';
 import { useMediaQuery } from 'react-responsive';
+import { useNavigate } from 'react-router-dom';
 
 import Icon from 'shared/components/Icon';
 import Calendar from 'components/Calendar';
@@ -19,6 +20,7 @@ const AddTransactionForm = () => {
   const [date, setDate] = useState(Date.now());
 
   const [postTransaction, { isSuccess }] = usePostTransactionMutation();
+  const navigate = useNavigate();
 
   const handleChange = ({ target }) => {
     const { name, value } = target;
@@ -27,7 +29,7 @@ const AddTransactionForm = () => {
   };
 
   const handleSubmit = e => {
-    e.preventDefault();
+    // e.preventDefault();
 
     const dataType =
       e.target.elements.category.value === 'income' ||
@@ -49,6 +51,7 @@ const AddTransactionForm = () => {
         category: '',
         sum: '',
       });
+    navigate('/');
   };
 
   const handleClear = () => {
